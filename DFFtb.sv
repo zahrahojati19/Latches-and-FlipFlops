52334dcb0be9a658464e0e644fdbe883fe6d7274
zahrahojati19 <129644190+zahrahojati19@users.noreply.github.com>
2023-06-02 10:59:24 +0330

    Add files via upload

---
+++ DFFtb.sv
@@ -0,0 +1,17 @@
+module dfftb();
+     logic clk,D,rst;
+     wire Q;
+     DFF tb(.clk(clk),.D(D),.rst(rst),.Q(Q));
+     always #83 clk=!clk;
+     initial begin
+     $monitor(clk,D,Q);
+     rst=0;
+     clk=0;D=1;#203
+     D=0;#203
+     D=1;#203
+     rst=1;#203
+     D=0;#203
+     D=1;#203
+     $stop;
+     end 
+endmodule
